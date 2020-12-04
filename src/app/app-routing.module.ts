16f835c76e9111a7f29135dd79facf7251cc038b
Stimmi <vincentreynaert88@gmail.com>
2020-12-04 23:20:11 +0100

    app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { RegisterComponent } from './register/register.component';
+import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 
 const routes: Routes = [
-  { path: 'game', component: GameComponent }
+  { path: '', redirectTo: '/registreer', pathMatch: 'full' },
+  { path: 'registreer', component: RegisterComponent },
+  { path: 'start', component: HomeComponent },
+  { path: 'score', component: ScoreboardComponent },
+  { path: 'schifting', component: GameComponent },
+  { path: 'quiz', component: QuizComponent }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
+import { ScoreboardComponent } from './scoreboard/scoreboard.component';
+import { RegisterComponent } from './register/register.component';
+import { MenuComponent } from './menu/menu.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    HomeComponent,
+    ScoreboardComponent,
+    RegisterComponent,
+    MenuComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
